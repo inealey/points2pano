8062a5c3f15fbaa34305161c66152ae74a3ae24a
Isaac Nealey <inealey@ucsd.edu>
2023-02-07 18:59:07 -0800

    vectorized code, bump up skybox sizes

--- points2pano.py
+++ points2pano.py
@@ -6,7 +6,7 @@
 
 import laspy
 import numpy as np
-from math import floor, sqrt, atan2, acos, pi
+from math import floor, pi
 import cv2
 import argparse
 from tqdm import tqdm
@@ -15,8 +15,10 @@ from multiprocessing import Process, shared_memory, cpu_count
 # constants
 NUM_WORKERS = cpu_count()
 CANVAS_ARRAY_NAME = 'npsharedcanvas'
-SKYBOX_PATH = 'skybox/skybox2.jpg'
-PRECISION = 12 ## how far to bitshift when plotting pts
+SKYBOX_PATH = 'skybox/skybox4.jpg'
+PRECISION = 16 ## how far to bitshift when plotting pts
+## 16 is approx precision of a 64-bit float
+FACTOR = 2 ** PRECISION ## bitshift factor (sig figs)
 
 ## create shared memory block from numpy-like object
 ## TODO: still some issues with shm on windows
@@ -37,62 +39,49 @@ def release_shared(name):
 ## return spherical coordinates and
 ## preserve intensity and color info
 ## inputs: 
-## las: laspy.lasdata.LasData object - from laspy.read()
-## canvas_shape: canvas dimensions
-## pts_size: point size multiplier
-## r_min: minimum aboslute distance
-## r_max: maximum absolute distance
-## img_type: string to decide how to color points
 ## start: index into arrays, starting point for reading/writing
 ## length: how far into the arrays to read/write
-def projectPoints(las, canvas_shape, pts_size, r_min, r_max, img_type, start, length):
-    
-    ## access the shared arrays
+## canvas_shape: canvas dimensions
+## theta: array of theta values
+## phi: array of phi values
+## radius: array of radii
+## color: array of BGR colors
+def projectPoints(start, length, canvas_shape, theta, phi, radius, color):
+    ## access the shared array
     shm_output = shared_memory.SharedMemory(name=CANVAS_ARRAY_NAME)
     image = np.ndarray(canvas_shape, dtype=np.uint8, buffer=shm_output.buf)
-    factor = 2 ** PRECISION
-    
-    ## project each point
+    ## project each point,
+    ## draw pts with subpixel coordinates
     for i in tqdm(range(start, start+length)):
-        r = sqrt(las.x[i] ** 2 + las.y[i] ** 2 + las.z[i] ** 2) # absolute distance
-        r_norm = (r - r_min) / (r_max - r_min) ## normalized distance. 0=close 1=far
-        radius = int(factor * pts_size * (1 - r_norm) + 1) ## linear falloff TODO better nonlinear function? seems ok for 4k
-        
-        if img_type == 'BGR': ## standard BGR
-            color = (int(las.blue[i]), int(las.green[i]), int(las.red[i]))
-        elif img_type == 'intensity': ## as an intensity image
-            color = (int(las.intensity[i] * 255), int(las.intensity[i] * 255), int(las.intensity[i] * 255))
-        elif img_type == 'distance': ## as a distance image
-            color = (int(r_norm * 255), int(r_norm * 255), int(r_norm * 255))
-        
-        ## draw pts with subpixel coordinates
         cv2.circle(image,
-            (int(round(((atan2(las.y[i], las.x[i]) + pi) /  (2 * pi) * image.shape[1]) * factor)), # x coordinate theta
-            int(round((acos(las.z[i] / r) / pi * image.shape[0]) * factor))), # y coordinate phi
-            radius = radius,
-            color = color,
-            thickness = cv2.FILLED, ## fill in circles
-            shift = PRECISION ) ## shift over
+            (theta[i], phi[i]),
+            radius = radius[i],
+            color = (int(color[i][0]), int(color[i][1]), int(color[i][2])),
+            thickness = cv2.FILLED,
+            shift = PRECISION )
         
-
 if __name__ == '__main__':
     ## set up argparser
     parser = argparse.ArgumentParser(
         prog = 'points2pano',
         description = 'project point clouds to 360 photo sphere' )
-    parser.add_argument('-i', '--input', type=str, default='example.las', help='path to .las/.laz LiDAR point cloud file')
-    parser.add_argument('-o', '--output', type=str, default='output.png', help='name of output image file')
-    parser.add_argument('-n', '--threads', type=int, default=1, help='number of threads')
-    parser.add_argument('-y', '--height', type=int, default=3840, help='size (height) of the output image. width = 2 * height')
-    parser.add_argument('-s', '--size', type=int, default=2, help='point radius in pixels')
-    parser.add_argument('-t', '--type', type=str, default='BGR', help='image type.')
+    parser.add_argument('-i', '--input', type=str, default='example.las', 
+                help='path to .las/.laz LiDAR point cloud file')
+    parser.add_argument('-o', '--output', type=str, default='output.png', 
+                help='name of output image file')
+    parser.add_argument('-n', '--threads', type=int, default=1, 
+                help='number of threads')
+    parser.add_argument('-l', '--log', type=str, default='WARNING', 
+                help='logging level')
+    parser.add_argument('-y', '--height', type=int, default=4320,
+                help='size (height) of the output image. width = 2 * height')
+    parser.add_argument('-s', '--size', type=int, default=3, 
+                help='point radius in pixels')
     parser.add_argument('--skybox', action='store_true', default=True)
     parser.add_argument('--no-skybox', dest='skybox', action='store_false')
     args = parser.parse_args()
     
     if args.threads > NUM_WORKERS: exit('too many threads requested')
-    if args.type != 'BGR' and args.type != 'distance' and args.type != 'intensity':
-        exit('did not recognize requested image type')
         
     ## read point cloud
     las = laspy.read(args.input)
@@ -102,12 +91,7 @@ if __name__ == '__main__':
     procs = [] ## process list
     height = args.height
     width = args.height * 2
-    
-    ## compute and normalize distance vector
-    r = np.sqrt(np.array(las.x) ** 2 + np.array(las.y) ** 2 + np.array(las.z) ** 2) ## absolute distance
-    r_min = min(r)
-    r_max = max(r)
-    del r
+    point_count = las.header.point_count
     
     ## skybox or black background.
     if not args.skybox:
@@ -119,29 +103,58 @@ if __name__ == '__main__':
         except cv2.error as e:
             print(e)
             background = np.zeros([height, width, 3])
+            
+    ## compute spherical coordinates as vectors
+    r = np.sqrt(np.array(las.x) ** 2 + np.array(las.y) ** 2 +
+                np.array(las.z) ** 2) ## absolute distances
+    
+    r_norm = (r - min(r)) / (max(r) - min(r))  ## normalized. 0=close 1=far.
+    
+    theta = np.array(((np.arctan2(las.y, las.x) + pi) /
+                      (2 * pi) * width) * FACTOR, dtype = np.uint64) # x value
+    
+    phi = np.array((np.arccos(las.z / r) / pi * height) *
+                   FACTOR, dtype = np.uint64) # y value
+    
+    ## radius has linear falloff TODO better nonlinear function?
+    radius = np.array(FACTOR * args.size * (1 - r_norm) + 1,
+                      dtype = np.uint64) # size of glyph
+    
+    color = np.swapaxes(np.array((las.blue, las.green, las.red),
+                                 dtype = np.uint8), 0, 1) # color of glyph
+    
+    del las ## explicit free, don't need point cloud anymore
     
     ## allocate shared memory for output array and distance vector
-    shm_output = create_shared_memory_nparray(background, CANVAS_ARRAY_NAME, np.uint8)
+    shm_output = create_shared_memory_nparray(background,
+                                              CANVAS_ARRAY_NAME, np.uint8)
     
     ## project to spherical coordinates
     ## compute start and length for indexing points array 
     for i in range(args.threads):
-        start = floor(i * las.header.point_count / args.threads)
-        length = floor(las.header.point_count * (i + 1) / args.threads) - floor(las.header.point_count * i / args.threads)
-        p = Process(target = projectPoints, args = (las, out_shape, args.size, r_min, r_max, args.type, start, length))
+        start = floor(i * point_count / args.threads)
+        length = floor(point_count * (i + 1) / args.threads) - \
+            floor(point_count * i / args.threads)
+        
+        p = Process(target = projectPoints,
+                args = (start, length, out_shape, theta, phi, radius, color))
+        
         procs.append(p)
         p.start()
     
     # complete the projection processes
-    for proc in procs:
-        proc.join()
+    for p in procs:
+        p.join()
     
     ## access the shared memory
     image = np.ndarray(out_shape, dtype=np.uint8, buffer=shm_output.buf)
     
     ## save image
-    cv2.imwrite(args.output, image)
-    
+    try:
+        cv2.imwrite(args.output, image)
+    except cv2.error as e:
+        print(e)
+            
     ## clean up shm
     release_shared(CANVAS_ARRAY_NAME)
     
